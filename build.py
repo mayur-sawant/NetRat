15072f2c457c9b544d53ee24957a59bb4228c84b
Mayur Sawant <mayursawant565@gmail.com.com>
2024-11-26 09:42:33 -0500

    build file

---
+++ build.py
@@ -0,0 +1,8 @@
+#!/user/bin/env python3
+#http://github.com/mayur-sawant/Packet-Sniffer
+
+__author__="mayur-sawant"
+
+import PyInstaller.__main__ as pyinstaller 
+
+ptinstaller.run(("packet sniffer","--onefile"))
